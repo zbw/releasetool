c59883c44c41e0198193d8b0d4af3a357c98b48d
Moritz Fürneisen <m.fuerneisen@zbw.eu>
2022-01-27 13:17:24 +0000

    Add text query to example script for simple SPARQL endpoint.

--- simple_sparql_service/query.py
+++ simple_sparql_service/query.py
@@ -16,14 +16,48 @@ SELECT * WHERE {
   FILTER (lang(?label) = "en")
 }
 """
+Q_AUTOCOMPLETE_FUZZY = """
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+
+PREFIX text: <http://jena.apache.org/text#>
+
+SELECT DISTINCT ?c ?prefLabel ?literal ?score
+WHERE {{
+  (?c ?score ?literal)
+     text:query ("{autocomplete_string}" ) .
+  ?c a skos:Concept, <{descriptor_type}> ;
+     skos:prefLabel ?prefLabel .
+  FILTER (lang(?prefLabel) IN ( {languages} ))
+}}
+ORDER BY DESC(?score) DESC(lang(?prefLabel))
+"""
 c_uris = " ".join(
         [
             "(<http://zbw.eu/stw/descriptor/{}>)".format(i)
             for i
             in ["19317-4", "10465-6"]])
-url = "http://127.0.0.1:5000/"
+url = "http://127.0.0.1:5000/stw"
 query = QT_LABELS % c_uris 
 result = requests.post(url, data={"query": query}, timeout=_TIMEOUT_S)
-result_json = json.loads(result.text)
-print(result_json)
-print(result.status_code, result.elapsed.total_seconds())
+if result.status_code == 200:
+    result_json = json.loads(result.text)
+    print(result_json)
+    print(result.status_code, result.elapsed.total_seconds())
+else:
+    print(f"ERROR {result.status_code}: {result.text}")
+
+query = Q_AUTOCOMPLETE_FUZZY.format(
+    descriptor_type='http://zbw.eu/stw/descriptor', 
+    autocomplete_string="Rente", 
+    languages=", ".join(["\""+ lang +"\"" for lang in ["en", "de"]]))
+result = requests.post(url, data={"query": query}, timeout=_TIMEOUT_S)
+if result.status_code == 200:
+    result_json = json.loads(result.text)
+    print(result_json)
+    print(result.status_code, result.elapsed.total_seconds())
+else:
+    print(f"ERROR {result.status_code}: {result.text}")
